aae5465c61540e22935a5026a0c62757a0d11662
Alhaji-LeoBoy <149122873+Alhaji-LeoBoy@users.noreply.github.com>
2023-12-11 20:23:15 +0100

    Delete js directory

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const btn = document.getElementById("menu");
-const container = document.querySelector(".navbar-container");
-btn.addEventListener("click", function () {
-  container.classList.toggle("high");
-});
